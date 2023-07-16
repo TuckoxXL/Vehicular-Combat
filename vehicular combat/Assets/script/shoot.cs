7b100e6d791c8f8f4c240c89eb7d321ee1735a29
TuckoxXL <sebastianbelluccini@gmail.com>
2023-07-16 20:10:02 -0300

    mejoras

---
+++ vehicular combat/Assets/script/shoot.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnBullet;
+    public float shootForce;
+    public float shootRate;
+    public float shootRateTime;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Time.time > shootRateTime)
+            {
+                GameObject newbullet;
+
+                newbullet = Instantiate(bullet, spawnBullet.position, spawnBullet.rotation);
+
+                newbullet.GetComponent<Rigidbody>().AddForce(spawnBullet.forward * shootForce, ForceMode.Impulse);
+
+                shootRateTime = Time.time + shootRate;
+
+                Destroy(newbullet, 2);
+
+            }
+        }
+    }
+}
