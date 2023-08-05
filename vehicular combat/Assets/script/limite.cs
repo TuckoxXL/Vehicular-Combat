801b24ebc334abb13bff053e15381a26c0bc4a39
TuckoxXL <sebastianbelluccini@gmail.com>
2023-08-05 18:19:18 -0300

    cosas agregadas
    se agrego el fondo de cielo junto con algunos scripts

---
+++ vehicular combat/Assets/script/limite.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class limite : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player1"))
+        {
+            SceneManager.LoadScene("");
+        }
+
+        if (other.gameObject.CompareTag("Player2"))
+        {
+            SceneManager.LoadScene("");
+        }
+    }
+
+}
